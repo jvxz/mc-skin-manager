e3c967c390d6280f7a5dff69a227f5d1beb074f9
jamie 🌨️ <jamie@wisp.bio>
2025-07-09 14:41:22 -0400

    refactor: font

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -1,11 +1,12 @@
-import { Geist_Mono, Inter } from 'next/font/google'
+import { IBM_Plex_Mono, Inter } from 'next/font/google'
 
 export const sans = Inter({
   subsets: ['latin'],
   variable: '--next-sans',
 })
 
-export const mono = Geist_Mono({
+export const mono = IBM_Plex_Mono({
   subsets: ['latin'],
   variable: '--next-mono',
+  weight: ['100', '200', '300', '400', '500', '600', '700'],
 })
