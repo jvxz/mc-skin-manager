101107de90c1989b61165672cf36878f724eb9fd
jamie 🌨️ <jamie@wisp.bio>
2025-07-09 15:45:35 -0400

    feat: virtual skin list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { Virtuoso } from 'react-virtuoso'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import {
@@ -8,37 +10,44 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 
+const data = Array.from({ length: 120 })
+
 export default function Page() {
   return (
     <div className="flex size-full">
-      <div className="w-1/4 border-r">
+      <div className="flex h-full min-h-0 w-1/4 flex-col border-r">
         <div className="flex items-center gap-2 border-b p-3">
           <Button>Upload</Button>
           <Input placeholder="Search..." />
         </div>
-        <div className="p-3">
-          {Array.from({ length: 8 }).map((_, i) => {
-            return (
-              <Card key={i} className="mb-3">
-                <div className="flex items-center gap-2">
-                  <Avatar className="size-[48px]" square>
-                    <AvatarImage src="https://github.com/jvxz.png" />
-                    <AvatarFallback>JV</AvatarFallback>
-                  </Avatar>
-                  <CardHeader>
-                    <CardTitle>Skin {i + 1}</CardTitle>
-                    <CardDescription>
-                      slim ⋅ uploaded 10 minutes ago
-                    </CardDescription>
-                  </CardHeader>
-                </div>
-                {/* <CardContent>
-              <p>Card Content</p>
-            </CardContent> */}
-              </Card>
-            )
-          })}
-        </div>
+        <Virtuoso
+          overscan={3}
+          data={data}
+          className="box-border h-full"
+          components={{
+            Item: ({ children, ...props }) => (
+              <div {...props} className="m-3">
+                {children}
+              </div>
+            ),
+          }}
+          itemContent={index => (
+            <Card className="">
+              <div className="flex items-center gap-2">
+                <Avatar className="size-[48px]" square>
+                  <AvatarImage src="https://github.com/jvxz.png" />
+                  <AvatarFallback>JV</AvatarFallback>
+                </Avatar>
+                <CardHeader>
+                  <CardTitle>Skin {index}</CardTitle>
+                  <CardDescription>
+                    slim ⋅ uploaded 10 minutes ago
+                  </CardDescription>
+                </CardHeader>
+              </div>
+            </Card>
+          )}
+        />
       </div>
       <div className="w-full "></div>
     </div>
