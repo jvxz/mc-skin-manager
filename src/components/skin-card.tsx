1a1b44eb20aa312e23db0160002c77e6d7903210
jamie 🌨️ <jamie@wisp.bio>
2025-07-10 18:35:46 -0400

    feat: enhance skin card

--- src/components/skin-card.tsx
+++ src/components/skin-card.tsx
@@ -1,3 +1,4 @@
+import { IconExternalLink } from '@tabler/icons-react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import {
   Card,
@@ -5,6 +6,13 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuTrigger,
+} from '@/components/ui/context-menu'
 import { useActiveSkin } from '@/hooks/active-skin'
 import type { Skin } from '@/lib/types'
 
@@ -16,27 +24,54 @@ function SkinCard({ skin }: { skin: Skin }) {
   }
 
   return (
-    <Card onClick={handleClick} className="hover:border-primary">
-      <div className="flex items-center gap-2">
-        <Avatar className="size-[48px]" square>
-          {skin.type === 'file' ? (
-            <AvatarImage src={URL.createObjectURL(skin.file)} />
-          ) : (
-            <AvatarImage src={skin.skinUrl} />
-          )}
-          <AvatarFallback className="animate-pulse bg-muted"></AvatarFallback>
-        </Avatar>
-        <CardHeader>
-          <CardTitle>{getSkinText(skin)}</CardTitle>
-          <CardDescription>slim ⋅ uploaded 10 minutes ago</CardDescription>
-        </CardHeader>
-      </div>
-    </Card>
+    <ContextMenu>
+      <ContextMenuTrigger asChild>
+        <Card onClick={handleClick} className="hover:border-primary">
+          <div className="flex items-center gap-2">
+            <Avatar className="size-[48px]" square>
+              {skin.inputType === 'file' ? (
+                <AvatarImage src={URL.createObjectURL(skin.file)} />
+              ) : (
+                <AvatarImage src={skin.skinUrl} />
+              )}
+              <AvatarFallback className="animate-pulse bg-muted"></AvatarFallback>
+            </Avatar>
+            <CardHeader>
+              <CardTitle title={getSkinText(skin)} className="truncate">
+                {getSkinText(skin)}
+              </CardTitle>
+              <CardDescription>
+                {/* {skin.skinType} ⋅ uploaded 10 minutes ago */}
+                {skin.skinType}
+              </CardDescription>
+            </CardHeader>
+          </div>
+        </Card>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem>Apply</ContextMenuItem>
+        <ContextMenuItem>Rename</ContextMenuItem>
+        <ContextMenuItem variant="destructive">Delete</ContextMenuItem>
+        <ContextMenuSeparator />
+        <ContextMenuItem>Copy URL</ContextMenuItem>
+
+        {skin.inputType === 'username' || skin.inputType === 'uuid' ? (
+          <>
+            <ContextMenuItem>Copy Username</ContextMenuItem>
+            <ContextMenuItem>Copy UUID</ContextMenuItem>
+            <ContextMenuSeparator />
+            <ContextMenuItem>
+              Open in NameMC <IconExternalLink />
+            </ContextMenuItem>
+          </>
+        ) : null}
+      </ContextMenuContent>
+    </ContextMenu>
   )
 }
 
 function getSkinText(skin: Skin) {
-  switch (skin.type) {
+  switch (skin.inputType) {
     case 'file':
       return skin.file.name
 
