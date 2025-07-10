3fd79e52f5bb697f168f06b1464d800eb6dbffd4
jamie 🌨️ <jamie@wisp.bio>
2025-07-10 18:36:05 -0400

    refactor: skin fetcher

---
+++ src/actions/get-skin-from-input.ts
@@ -0,0 +1,93 @@
+'use server'
+import * as mc from 'minecraft-api-wrapper'
+import type { Skin, SkinInputType } from '@/lib/types'
+import { getSkinType } from './get-skin-type'
+
+type Params = {
+  type: SkinInputType
+  input: string | File
+}
+
+export async function getSkinFromInput({ type, input }: Params): Promise<Skin> {
+  switch (type) {
+    case 'username': {
+      const profile = await mc.getProfileFromUsername(input as string)
+
+      if (!profile) throw new Error('Profile not found from username')
+
+      const url = await profile.getSkinUrl()
+
+      if (!url) throw new Error('Username has no skin')
+
+      const uuid = profile.getUUID()
+
+      const skinType = await getSkinType({
+        url,
+      })
+
+      return {
+        id: crypto.randomUUID(),
+        inputType: 'username',
+        skinType,
+        skinUrl: url,
+        username: input as string,
+        uuid,
+      }
+    }
+    case 'uuid': {
+      const profile = await mc.getProfileFromUUID(input as string)
+
+      if (!profile) throw new Error('Profile not found from UUID')
+
+      const url = await profile.getSkinUrl()
+
+      if (!url) throw new Error('UUID has no skin')
+
+      const username = profile.getName()
+
+      const skinType = await getSkinType({
+        url,
+      })
+
+      return {
+        id: crypto.randomUUID(),
+        inputType: 'uuid',
+        skinType,
+        skinUrl: url,
+        username,
+        uuid: input as string,
+      }
+    }
+    case 'url': {
+      const skinType = await getSkinType({
+        url: input as string,
+      })
+
+      return {
+        id: crypto.randomUUID(),
+        inputType: 'url',
+        skinType,
+        skinUrl: input as string,
+      }
+    }
+    case 'file': {
+      const skinType = await getSkinType({
+        url: URL.createObjectURL(input as File),
+      })
+
+      return {
+        file: input as File,
+        id: crypto.randomUUID(),
+        inputType: 'file',
+        skinType,
+      }
+    }
+    default:
+      return {
+        id: crypto.randomUUID(),
+        inputType: 'url',
+        skinType: 'classic',
+        skinUrl: input as string,
+      }
+  }
+}
