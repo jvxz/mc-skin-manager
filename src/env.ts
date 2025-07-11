c0407b8a6e1d763497f079f7627876858a1bec59
jamie 🌨️ <jamie@wisp.bio>
2025-07-11 15:07:07 -0400

    refactor: use t3-env for env validation

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,15 @@
-import { z } from 'zod'
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod/v4-mini'
 
-const envSchema = z.object({
-  DATABASE_URL: z.string(),
+export const env = createEnv({
+  runtimeEnv: {
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
+  server: {
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.url(),
+    DATABASE_URL: z.url(),
+  },
 })
-
-export const env = envSchema.parse(process.env)
