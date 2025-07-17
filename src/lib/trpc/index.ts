117b7913f23c0fa86e19f4dcc1138f7cbd9fadbf
jamie 🌨️ <jamie@wisp.bio>
2025-07-17 18:33:35 -0400

    feat: trpc logging middleware

--- src/lib/trpc/index.ts
+++ src/lib/trpc/index.ts
@@ -3,4 +3,11 @@ import { initTRPC } from '@trpc/server'
 const t = initTRPC.create()
 
 export const router = t.router
-export const publicProcedure = t.procedure
+export const publicProcedure = t.procedure.use(async opts => {
+  const result = await opts.next()
+
+  // biome-ignore lint/suspicious/noConsole: server only
+  console.debug(result)
+
+  return result
+})
