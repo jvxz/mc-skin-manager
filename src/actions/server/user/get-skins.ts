89f4ad6edd5ed0e92334eabd51122747229feb29
jamie 🌨️ <jamie@wisp.bio>
2025-07-18 16:36:15 -0400

    feat: user skin getter

--- src/actions/server/user/get-skins.ts
+++ src/actions/server/user/get-skins.ts
@@ -1,8 +1,15 @@
 'use server'
+import { eq } from 'drizzle-orm'
+import { db } from '@/db'
+import { skins } from '@/db/schema'
 import { getAuthData } from '../utils/get-auth-data'
 
 export async function getSkins() {
   const authData = await getAuthData()
 
-  return authData.user
+  const userSkins = await db.query.skins.findMany({
+    where: eq(skins.userId, authData.user.id),
+  })
+
+  return userSkins
 }
