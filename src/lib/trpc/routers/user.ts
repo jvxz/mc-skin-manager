e3b55a51d3c0b7139ed607d91537d904e498ff54
jamie 🌨️ <jamie@wisp.bio>
2025-07-19 15:58:07 -0400

    refactor: rename server actions to categorize

--- src/lib/trpc/routers/user.ts
+++ src/lib/trpc/routers/user.ts
@@ -1,8 +1,8 @@
-import { getSkins } from '@/actions/server/user/get-skins'
+import { getUserSkins } from '@/actions/server/user/get-skins'
 import { publicProcedure, router } from '..'
 
 export const userRouter = router({
-  getSkins: publicProcedure.query(async () => await getSkins()),
+  getSkins: publicProcedure.query(async () => await getUserSkins()),
 })
 
 export type UserRouter = typeof userRouter
