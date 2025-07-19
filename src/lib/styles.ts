afd19f062cd9e564ea3b8749899340233707cb43
jamie 🌨️ <jamie@wisp.bio>
2025-07-19 14:55:24 -0400

    refactor: global styles

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -1,5 +1,5 @@
 const interactiveStyles = {
-  base: 'cursor-pointer font-medium focus-visible:border-primary/50 select-none focus-visible:ring-ring/40 focus-visible:ring-[3px] active:ring-ring/60 aria-invalid:ring-destructive/20 aria-invalid:border-destructive dark:aria-invalid:ring-destructive/40 shrink-0 gap-2 rounded text-base whitespace-nowrap outline-none disabled:pointer-events-none hover:disabled:cursor-not-allowed disabled:opacity-50 underline-offset-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4 active:scale-[97%] transition-transform duration-150',
+  base: 'cursor-pointer font-medium select-none focus-visible:ring-ring/40 focus-visible:ring-[3px] active:ring-ring/60 aria-invalid:ring-destructive/20 aria-invalid:border-destructive dark:aria-invalid:ring-destructive/40 shrink-0 gap-2 rounded text-base whitespace-nowrap outline-none disabled:pointer-events-none hover:disabled:cursor-not-allowed disabled:opacity-50 underline-offset-4 [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg]:size-4 active:scale-[97%] transition-transform duration-150',
   size: {
     default: 'h-8 px-3 py-1.5 text-sm',
     icon: 'aspect-square size-8',
@@ -8,14 +8,15 @@ const interactiveStyles = {
   },
   variant: {
     default:
-      'bg-primary/85 border hover:bg-primary/90 border-primary active:bg-primary/85 text-primary-foreground glow-primary',
+      'bg-primary/85 border focus-visible:border-primary/50 hover:bg-primary/90 border-primary active:bg-primary/85 text-primary-foreground glow-primary',
     destructive:
       'hover:bg-destructive/90 bg-destructive/90 text-destructive-foreground hover:bg-destructive active:bg-destructive',
-    ghost: 'hover:bg-muted/90 active:bg-muted/80',
+    ghost:
+      'hover:bg-muted/90 active:bg-muted/80 text-muted-foreground hover:text-foreground',
     link: 'text-primary underline-offset-4 hover:underline',
     // shadow-[0px_-1px_0px_0px_hsl(from_var(--muted-foreground)_h_s_l/0.40),0px_1px_0px_0px_hsl(from_var(--muted)_h_s_l),inset_0px_-2px_0px_0px_hsl(from_var(--muted-foreground)_h_s_l/0.035)]
     outline:
-      'border-border/90 hover:bg-muted/90 active:bg-muted/80 hover:border-border active:border-border border shadow-[0px_-1px_0px_0px_hsl(from_var(--muted-foreground)_h_s_l/0.40),0px_1px_0px_0px_hsl(from_var(--muted)_h_s_l))] border-y-0 bg-transparent',
+      'border-border/90 text-muted-foreground hover:text-foreground hover:bg-muted/90 active:bg-muted/80 hover:border-border active:border-border border shadow-[0px_-1px_0px_0px_hsl(from_var(--muted-foreground)_h_s_l/0.40),0px_1px_0px_0px_hsl(from_var(--muted)_h_s_l))] border-y-0 bg-transparent',
   },
 }
 
@@ -23,7 +24,7 @@ const staticStyles = {
   base: 'rounded p-3',
   variant: {
     default:
-      'bg-card shadow-[0px_-1px_0px_0px_hsl(from_var(--muted-foreground)_h_s_l/0.40),0px_1px_0px_0px_hsl(from_var(--muted)_h_s_l))] border border-y-0 text-card-foreground',
+      'bg-card shadow-[0px_-1px_0px_0px_hsl(from_var(--muted-foreground)_h_s_l/0.25),0px_1px_0px_0px_hsl(from_var(--muted)_h_s_l),0px_8px_12px_0px_hsl(from_var(--background)_h_s_l/0.5)] border border-y-0 text-card-foreground',
     destructive:
       'bg-card border-destructive text-destructive border [&>svg]:text-current',
   },
@@ -44,6 +45,6 @@ const popoverStyles = {
 }
 
 const overlayStyles =
-  'data-[state=open]:animate-in data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-overlay backdrop-blur-xs'
+  'data-[state=open]:animate-in data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-overlay'
 
 export { interactiveStyles, overlayStyles, popoverStyles, staticStyles }
