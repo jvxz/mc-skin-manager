d2113abfd1fbd8b9c9f88ae551d52c10bd03f5d9
jamie 🌨️ <jamie@wisp.bio>
2025-07-19 16:32:18 -0400

    feat: global local migration alert

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic'
 import type { ThemeProviderProps } from 'next-themes'
 import { NuqsAdapter } from 'nuqs/adapters/next'
 import { TRPCReactProvider } from '@/lib/trpc/client'
+import { LocalMigrationAlert } from './alerts/local-migration-alert'
 import { Toaster } from './ui/sonner'
 
 // prevent hydration errors
@@ -25,6 +26,7 @@ function Providers({ children, ...props }: ThemeProviderProps) {
         {...props}>
         <NuqsAdapter>{children}</NuqsAdapter>
         <Toaster />
+        <LocalMigrationAlert />
       </NextThemesProvider>
       <ReactQueryDevtools />
     </TRPCReactProvider>
