cda0c25e6eeae76fccbeb6804693a2be41f566ec
jamie 🌨️ <jamie@wisp.bio>
2025-07-19 16:31:55 -0400

    feat: skin migration server action

---
+++ src/actions/server/user/migrate-skins.ts
@@ -0,0 +1,18 @@
+'use server'
+import { db } from '@/db'
+import { type Skin, skins } from '@/db/schema'
+import { getAuthData } from '../utils/get-auth-data'
+
+export async function migrateLocalSkinsToUser(localSkins: Skin[]) {
+  const authData = await getAuthData()
+
+  const patchedLocalSkins = localSkins.map(skin => ({
+    ...skin,
+    createdAt: new Date(skin.createdAt),
+    userId: authData.user.id,
+  }))
+
+  await db.insert(skins).values(patchedLocalSkins)
+
+  return patchedLocalSkins
+}
