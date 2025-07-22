431273bdaf5d9fb4784efa9edf6bc248126b8044
jamie 🌨️ <jamie@wisp.bio>
2025-07-22 15:44:00 -0400

    refactor: remove expanding sidebar functionality

--- src/components/inset-buttons.tsx
+++ src/components/inset-buttons.tsx
@@ -1,16 +1,9 @@
-import {
-  IconLayoutSidebar,
-  IconLibrary,
-  IconSettings,
-} from '@tabler/icons-react'
+import { IconLibrary, IconSettings } from '@tabler/icons-react'
 import Link from 'next/link'
-import { useExpanded } from '@/hooks/use-expanded'
 import { InsetButtonsAuth } from './inset-buttons-auth'
 import { Button } from './ui/button'
 
 function InsetButtons() {
-  const { expanded, setExpanded } = useExpanded()
-
   return (
     <div className="absolute inset-0 m-2 w-[calc(var(--expanded)-1.5rem)]">
       <div className="flex flex-col gap-2">
@@ -24,12 +17,6 @@ function InsetButtons() {
           <Link href="/settings">
             <IconSettings className="!size-8" />
           </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          onClick={() => setExpanded(!expanded)}
-          className="size-16">
-          <IconLayoutSidebar className="!size-8" />
         </Button>
       </div>
     </div>
