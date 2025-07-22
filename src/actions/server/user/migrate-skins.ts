d0efe20c64a7dda749cf38462cd9d91cddf9d074
jamie 🌨️ <jamie@wisp.bio>
2025-07-22 00:31:21 -0400

    refactor: update local skin migrator

--- src/actions/server/user/migrate-skins.ts
+++ src/actions/server/user/migrate-skins.ts
@@ -2,6 +2,7 @@
 import { db } from '@/db'
 import { type Skin, skins } from '@/db/schema'
 import { getAuthData } from '../utils/get-auth-data'
+import { postSkinToUT } from '../utils/post-skin-to-ut'
 
 export async function migrateLocalSkinsToUser(localSkins: Skin[]) {
   const authData = await getAuthData()
@@ -10,11 +11,26 @@ export async function migrateLocalSkinsToUser(localSkins: Skin[]) {
     throw new Error('Unauthorized')
   }
 
-  const patchedLocalSkins = localSkins.map(skin => ({
-    ...skin,
-    createdAt: new Date(skin.createdAt),
-    userId: authData.user.id,
-  }))
+  const patchedLocalSkins: Skin[] = await Promise.all(
+    localSkins.map(async skin => {
+      if (skin.skinUrl === '') {
+        const skinUrl = await postSkinToUT(skin)
+
+        return {
+          ...skin,
+          createdAt: new Date(skin.createdAt),
+          skinUrl,
+          userId: authData.user.id,
+        }
+      }
+
+      return {
+        ...skin,
+        createdAt: new Date(skin.createdAt),
+        userId: authData.user.id,
+      }
+    }),
+  )
 
   await db.insert(skins).values(patchedLocalSkins)
 
