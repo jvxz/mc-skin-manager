8835b9857e038ad523f9e7c755c641c632767818
jamie 🌨️ <jamie@wisp.bio>
2025-07-23 13:58:29 -0400

    feat: add auth data to useUser hook

--- src/hooks/use-user.ts
+++ src/hooks/use-user.ts
@@ -2,12 +2,15 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { toast } from 'sonner'
 import { getUserMojangData } from '@/actions/server/user/get-user-mojang-data'
 import { unlinkUserMicrosoftAccount } from '@/actions/server/user/unlink-user-microsoft-account'
+import { useSession } from '@/auth/client'
 
 const MOJANG_DATA_KEY = ['user-mojang-data']
 const UNLINK_MICROSOFT_ACCOUNT_KEY = ['unlink-user-microsoft-account']
 
 export function useUser() {
   const qc = useQueryClient()
+
+  const { data: authData, isPending: isLoadingAuthData } = useSession()
 
   const { data: mojangData, isLoading: isLoadingMojangData } = useQuery({
     queryFn: getUserMojangData,
@@ -26,10 +29,11 @@ export function useUser() {
     },
   })
 
-  const isLoading = isLoadingMojangData
+  const isLoading = isLoadingMojangData || isLoadingAuthData
   const isMutating = isUnlinkingMicrosoftAccount
 
   return {
+    authData,
     isLoading,
     isMutating,
     mojangData,
