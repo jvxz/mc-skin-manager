bcbeb525af79e51a5f1cf8086f3b8e851bc24562
jamie 🌨️ <43821564+jvxz@users.noreply.github.com>
2025-07-30 18:01:19 -0400

    feat: server action to upload client-generated thumbnail to UT

---
+++ src/actions/server/utils/post-thumbnail-to-ut.ts
@@ -0,0 +1,19 @@
+'use server'
+import { UTApi } from 'uploadthing/server'
+
+export async function postThumbnailToUI(thumbnail: string) {
+  const ut = new UTApi()
+
+  const thumbnailFileBuffer = Buffer.from(thumbnail, 'base64')
+  const thumbnailFile = new File([thumbnailFileBuffer], 'thumbnail.png', {
+    type: 'image/png',
+  })
+
+  const uploadRes = await ut.uploadFiles(thumbnailFile)
+
+  if (uploadRes.error) {
+    throw new Error('Failed to upload thumbnail')
+  }
+
+  return uploadRes.data.ufsUrl
+}
